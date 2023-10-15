e1d13e21acdc0a7cecf141dd6092b19c4feea93a
vghulyan <vghulyan@yahoo.com>
2023-10-15 17:18:56 +0100

    Add a screenshot

--- kitchen-sink/src/components/Navbar.jsx
+++ kitchen-sink/src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
     <AppBar position="sticky">
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
-          LAMA DEV
+          KITCHEN SINK
         </Typography>
         <Pets sx={{ display: { xs: "block", sm: "none" } }} />
         <Search>
